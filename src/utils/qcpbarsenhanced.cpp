308c1746ff4d2ec75c416e458fe804a20fe0c7e8
taxus13 <taxus13+github@lostbit.de>
2015-06-06 23:09:33 +0200

    safsd

---
+++ src/utils/qcpbarsenhanced.cpp
@@ -0,0 +1,8 @@
+#include "qcpbarsenhanced.h"
+
+QCPBarsEnhanced::QCPBarsEnhanced(QCPAxis *keyAxis, QCPAxis *valueAxis) :
+    QCPBars(keyAxis, valueAxis)
+{
+
+}
+
---
+++ src/utils/qcpbarsenhanced.h
@@ -0,0 +1,15 @@
+#ifndef QCPBARSENHANCED_H
+#define QCPBARSENHANCED_H
+
+#include "qcustomplot.h"
+
+class QCPBarsEnhanced : public QCPBars
+{
+public:
+    QCPBarsEnhanced(QCPAxis *keyAxis, QCPAxis *valueAxis);
+
+signals:
+
+};
+
+#endif // QCPBARSENHANCED_H
